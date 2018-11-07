086d178ace0d554daee4e4c0d7ec4ede039c2cef
wnan0424 <43769295+wnan0424@users.noreply.github.com>
2018-11-07 12:12:27 -0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(lubridate)
+
+aa <- read.csv("household_power_consumption.txt",sep = ";", header = T,na.strings="?")
+data <- filter(aa, Date == c("1/2/2007", "2/2/2007"))
+data$Date <- dmy(data$Date)
+data <- mutate(data, wholetime = ymd_hms(paste(data$Date,data$Time,sep = " ")))
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+#plot 4 
+par(mfrow = c(2,2)) 
+plot(data$wholetime,data$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power")
+plot(data$wholetime,data$Voltage, type="l", col="black", xlab="datatime", ylab="Voltage")
+with(data, plot(wholetime, Sub_metering_1, type = "l", col = "black",xlab="", ylab="Energy sub metering"))
+points(data$wholetime,data$Sub_metering_2, type = "l",col = "red")
+points(data$wholetime,data$Sub_metering_3,type = "l" , col = "blue")
+legend("topright", col = c("black","red","blue"),lty=1, lwd=2,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(data$wholetime,data$Global_reactive_power, type="l", col="black", xlab="datatime", ylab="Global_reactive_power")
+dev.copy(png,"plot4.png", width=480, height=480)
+dev.off()
